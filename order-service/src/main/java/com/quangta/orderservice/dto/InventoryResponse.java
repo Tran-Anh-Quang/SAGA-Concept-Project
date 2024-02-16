b5888afea48d6119b5e2347952db10f2235a4e1b
dinos <quangta1@newwave.vn>
2024-02-16 22:10:02 +0700

    Completed communication between Inventory-Service & Order-Service

---
+++ order-service/src/main/java/com/quangta/orderservice/dto/InventoryResponse.java
@@ -0,0 +1,15 @@
+package com.quangta.orderservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryResponse {
+    private String skuCode;
+    private boolean isInStock;
+}
--- order-service/src/main/java/com/quangta/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/quangta/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.quangta.orderservice.service;
 
+import com.quangta.orderservice.dto.InventoryResponse;
 import com.quangta.orderservice.dto.OrderLineItemsDTO;
 import com.quangta.orderservice.dto.OrderRequest;
 import com.quangta.orderservice.entity.Order;
@@ -8,7 +9,10 @@ import com.quangta.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,6 +22,8 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+
+    private final WebClient webClient;
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -28,7 +34,26 @@ public class OrderService {
                 .toList();
         order.setOrderLineItemsList(orderLineItems);
 
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        //Call Inventory Service, and place order if product is in stock
+        InventoryResponse[] inventoryResponsesArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponsesArray)
+                .allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock){
+            orderRepository.save(order);
+        }else {
+            throw new IllegalStateException("Product is not in stock, please try again later");
+        }
     }
 
     private OrderLineItems mapToDTO(OrderLineItemsDTO orderLineItemsDTO) {
