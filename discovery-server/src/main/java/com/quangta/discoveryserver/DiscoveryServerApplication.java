8d3f63d488b30a2802b037b4247ff7546608d2b9
dinos <quangta1@newwave.vn>
2024-02-21 18:18:58 +0700

    Completed API gateway

--- discovery-server/src/main/java/com/quangta/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/quangta/discoveryserver/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package com.quangta;
+package com.quangta.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
